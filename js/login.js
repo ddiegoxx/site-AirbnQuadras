fc1c1eac79abe7b96fc65d01dd931642d21532f7
ddiegoxx <dm1592200@gmail.com>
2024-10-16 20:41:26 -0300

    parte do js do login

---
+++ js/login.js
@@ -0,0 +1,62 @@
+class Validator{
+
+    constructor() {
+        this.validations = [
+            'data-min-length',
+            'data-email-validate',
+            'data-required',
+            'data-password-validate',
+        ]
+    }
+    validate(form) {
+        let currentValidations = document.querySelectorAll('form .error-validation');
+
+        if(currentValidations.length) {
+            this.cleanValidations(currentValidations);
+        }
+
+        let inputss = form.getElementByIdTagName('input');
+
+        let inputsArrays = [...inputss];
+
+        inputsArrays.forEach(function(input, objs) {
+
+            for(let i = 0; this.validations.length >i; i++) {
+                if (input.getAttribute(this.validations[i]) !=null) {
+
+                    let method = this.validations[i].replace("data-", "").replace("-", "");
+                    
+                    let value = input.getAttribute(this.validations[i])
+
+                    this[method](input,value);
+                }
+            }
+            
+        }, this);
+    }
+    minlength(input, minValue){
+        let inputsLength = input.value.length;
+
+        let errorMenssage = 'O ca'
+    }
+
+    cleanValidations(validations) {
+        validations.forEach(ele => ele.remove());
+    }
+}
+
+
+
+
+
+let form1 = document.getElementById('form-login');
+let submit = document.getElementById('btn-submit');
+
+let validator = new Validator();
+
+submit.addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    validator.validate(form);
+});
